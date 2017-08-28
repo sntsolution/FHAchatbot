fb2101cbe0bdedd6cde0bfd97f3d5d79c01862ee
sntsolution <sntwpdevelopment@gmail.com>
2017-08-28 10:33:45 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
-$con = mysqli_connect("localhost","root","","fhachatbot");
+//$con = mysqli_connect("localhost","root","","fhachatbot");
 //$con = mysqli_connect("localhost","seorslrb_chatbot","Chatbot99*#","seorslrb_chatbot");
-//$con = mysqli_connect("us-cdbr-iron-east-03.cleardb.net","ba4caad30c8956","f1f9fbf1","heroku_c8d0aef7042f16f");
+$con = mysqli_connect("us-cdbr-iron-east-03.cleardb.net","ba4caad30c8956","f1f9fbf1","heroku_c8d0aef7042f16f");
 // Check connection
 if (mysqli_connect_errno())
   {
